5e8786ab22b83efcaf2434edc7952f1d567a6e02
apple1014 <20p1146@stu.meisei-u.ac.jp>
2026-09-05 00:30:36 +0900

    Update service worker cache name and assets

--- sw.js
+++ sw.js
@@ -1,23 +1,20 @@
-const CACHE_NAME = 'homework-app-v2';
+const CACHE_NAME = 'homework-app-v3';
 const ASSETS = [
   './',
   './index.html',
-  './html5-qrcode.min.js'
+  './html5-qrcode.min.js',
+  './qrcode.min.js'
 ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then((response) => response || fetch(e.request))
   );
 });
